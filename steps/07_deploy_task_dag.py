570004780cac2e224aa37d86e0ac2cb590e0ded4
nik333adi <141574811+nik333adi@users.noreply.github.com>
2024-01-31 07:38:29 +0000

    added utils

--- steps/07_deploy_task_dag.py
+++ steps/07_deploy_task_dag.py
@@ -108,19 +108,19 @@ def main(session: Session) -> str:
 
 # For local debugging
 # Be aware you may need to type-convert arguments if you add input parameters
-if __name__ == '__main__':
-    import os, sys
-    # Add the utils package to our path and import the snowpark_utils function
-    current_dir = os.getcwd()
-    parent_dir = os.path.dirname(current_dir)
-    sys.path.append(parent_dir)
+#if __name__ == '__main__':
+#    import os, sys
+#    # Add the utils package to our path and import the snowpark_utils function
+#    current_dir = os.getcwd()
+#    parent_dir = os.path.dirname(current_dir)
+#    sys.path.append(parent_dir)
 
-    from utils import snowpark_utils
-    session = snowpark_utils.get_snowpark_session()
-
-    if len(sys.argv) > 1:
-        print(main(session, *sys.argv[1:]))  # type: ignore
-    else:
-        print(main(session))  # type: ignore
-
-    session.close()
+#    from utils import snowpark_utils
+#    session = snowpark_utils.get_snowpark_session()
+#
+#    if len(sys.argv) > 1:
+#        print(main(session, *sys.argv[1:]))  # type: ignore
+#    else:
+#        print(main(session))  # type: ignore
+#
+#    session.close()
